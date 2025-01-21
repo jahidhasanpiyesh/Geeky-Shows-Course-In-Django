cd4f27ff7f280ff9937113b9e4ba7f55e5fcba95
Md. Jahid Hasan <122719439+jahidhasanpiyesh@users.noreply.github.com>
2025-01-21 07:40:03 +0600

    Delete Practice__04/First_Topic___include(module.namespace=None) directory
    solve it problem.

--- Practice__04/First_Topic___include(module.namespace=None)/homeapp2/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here. & one functions 
-def homeapp2(request):
-    return HttpResponse("Second in homeapp2")
